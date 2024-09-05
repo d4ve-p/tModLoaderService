b6d5ea3a9222e804e63536c5fb42ccb9c61d00f3
Dave P <65463983+d4ve-p@users.noreply.github.com>
2024-09-05 16:38:25 +0700

    Revert "prettify"
    This reverts commit 6d2210cb3d880fc416692161a64f913436aac3f7.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,65 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-import { socketServer } from "./service/ws_service";
-import { http } from "./service/http_service";
+import express, { Request, Response } from "express";
+import WebSocket from "ws"
+import { serverRunner } from "./service/server_service";
 
+const app = express();
+const socketServer = new WebSocket.Server({ port: Number(process.env.WS_PORT) });
 const port = process.env.PORT || 9696; 
 
 socketServer.on('listening', () => {
-    console.log("socket ready")
+    console.log("ready")
 })
 
-http.listen(port, () => {
-    console.log("http ready")
+socketServer.on('connection', ws => {
+    ws.send("connection established")
+
+    serverRunner.pushClient(ws);
+    
+    ws.on('message', (message) => {
+        try {
+            serverRunner.send(message.toString());
+            serverRunner.callClient((other) => {
+                if(other !== ws)
+                    other.send(`Anonymous sent a command: ${message.toString()}`)
+            })
+        } catch(error: any) {
+            ws.send(error.toString());
+        }
+        
+    })
+})
+
+app.use(express.json());
+
+app.post('/power', (req: Request, res: Response) => {
+    const power = req.body.power as string
+
+    if(power.toLocaleLowerCase() === "on") {
+        try {
+            serverRunner.start();
+            return res.json({ message: "Success "});
+        } catch(error: any) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
+    if(power.toLocaleLowerCase() === "off") {
+        try {
+            serverRunner.stop();
+            return res.json({ message: "Success" });
+        } catch(error: any) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
+    if(power.toLocaleLowerCase() === "status") {
+        return res.json({ message: serverRunner.isRunning() ? "On" : "Off" })
+    }
+
+    return res.status(400).json({ message: "Unknown command" });
 });
 
+app.listen(port, () => {
+
+});
